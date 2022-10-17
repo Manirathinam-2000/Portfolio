162723a2b2b4f4369c39fb1d26bc264e1fb27253
Manirathinam <manirathinam2000@gmail.com>
2022-10-18 01:14:01 +0530

    Initial test commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", ".ejs");
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+  // res.redirect("/")
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started successfully");
+});
