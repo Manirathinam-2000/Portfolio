1de0de3c17f8dd190ed106c8efc7783421ffa774
Manirathinam <manirathinam2000@gmail.com>
2022-10-27 10:03:32 +0530

    updated stule1-light and created ejs templates for header and footer and created style toggle button and added the required functionality

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ app.set("view engine", ".ejs");
 
 app.use(express.static("public"));
 
+let pageStyle = "style1-light.css";
+let btnTheme = "dark";
+let c = 0;
+
 app.get("/", function(req,res){
-  res.render("home");
+  res.render("home", {
+    stylesheetName : pageStyle,
+    btnTheme : btnTheme
+  });
 });
 
-
+app.post("/", function(req,res){
+  if(c===0){
+    pageStyle = "style1-dark.css";
+    btnTheme = "light";
+    c=1;
+  }
+  else{
+    pageStyle = "style1-light.css";
+    btnTheme = "dark";
+    c=0;
+  }
+  res.redirect("/");
+})
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
