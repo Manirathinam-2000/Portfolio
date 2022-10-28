bb8de99bba0c8a2d109296af2b19f93721c69c4d
Manirathinam <manirathinam2000@gmail.com>
2022-10-28 12:41:30 +0530

    added get functions and post functions and created new ejs files for navbar items

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const http = require("http");
+const url = require("url");
 const app = express();
 
 app.set("view engine", ".ejs");
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+let aURL = "";
 let pageStyle = "style1-light.css";
 let btnTheme = "dark";
-let lndImg = "MR_landing_img_lt.png"
+let lndImg = "MR_landing_img_lt.png";
 let c = 0;
 
 app.get("/", function(req,res){
   res.render("home", {
     stylesheetName : pageStyle,
     btnTheme : btnTheme,
+    actionUrl: aURL,
     landingImg: lndImg
   });
 });
-
+app.get("/:customPageURL", function(req,res){
+  aURL = _.replace(url.parse(req.url).pathname,"/","");
+  res.render(aURL, {
+    stylesheetName : pageStyle,
+    btnTheme : btnTheme,
+    actionUrl: aURL,
+    landingImg: lndImg
+  });
+});
 app.post("/", function(req,res){
   if(c===0){
     pageStyle = "style1-dark.css";
@@ -33,6 +48,22 @@ app.post("/", function(req,res){
     c=0;
   }
   res.redirect("/");
+})
+
+app.post("/:customPageURL", function(req,res){
+  if(c===0){
+    pageStyle = "style1-dark.css";
+    lndImg = "MR_landing_img_dr.png";
+    btnTheme = "light";
+    c=1;
+  }
+  else{
+    pageStyle = "style1-light.css";
+    lndImg = "MR_landing_img_lt.png";
+    btnTheme = "dark";
+    c=0;
+  }
+  res.redirect(url.parse(req.url).pathname);
 })
 let port = process.env.PORT;
 if (port == null || port == "") {
